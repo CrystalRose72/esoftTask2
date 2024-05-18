90a5b35ecf33c976241d59d80de7516309b040a3
CrystalRose72 <k.miheeva10@gmail.com>
2024-05-18 13:47:05 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card>
+          <img src='kot.jpg'></img>
+            <Card.Img width={150} src="kot.jpg" rounded variant="top"></Card.Img>
+            <Card.Title>Михеева Екатерина Андреевна</Card.Title>
+        </Card>
       </header>
     </div>
   );
